537d37aeee47fb6d8bd76beac8215d2b20005918
theekshana <rtheekshanafernando@gmail.com>
2023-02-21 19:10:30 +0530

    new

--- src/About.jsx
+++ src/About.jsx
@@ -6,39 +6,8 @@ const linkStyle = {
 };
 
 export default function App() {
-  const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-
-  let interval = null;
-
-  useEffect(() => {
-    document.querySelector(".name").addEventListener("mouseover", (event) => {
-      let iteration = 0;
-      clearInterval(interval);
-      interval = setInterval(() => {
-        event.target.innerText = event.target.innerText
-          .split("")
-          .map((letter, index) => {
-            if (index < iteration) {
-              return event.target.dataset.value[index];
-            }
-            return letters[Math.floor(Math.random() * 26)];
-          })
-          .join("");
-        if (iteration >= event.target.dataset.value.length) {
-          clearInterval(interval);
-        }
-        iteration += 1 / 3;
-      }, 30);
-    });
-  }, []);
-
   return (
     <>
-      <div className="fixed top-0 w-screen h-10 flex items-center justify-end">
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 text-white mr-24">
-          <path strokeLinecap="round" strokeLinejoin="round" d="M12 3v2.25m6.364.386l-1.591 1.591M21 12h-2.25m-.386 6.364l-1.591-1.591M12 18.75V21m-4.773-4.227l-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z" />
-        </svg>
-      </div>
       <div className="h-screen w-screen bg-[#111010] overflow-hidden font-serif z-40">
         {/*Hero*/}
         <div className="max-w-4xl mx-auto h-1/2 flex mt-36">
@@ -47,18 +16,16 @@ export default function App() {
             <div className="w-full h-20 flex items-center mb-5">
               <span className="text-5xl font-bold text-white font-kaisei">TF</span>
             </div>
-            <a href="" className={linkStyle.selected}>
+            <a href="/" className={linkStyle.normal}>
               home
             </a>
-            <a href="/about" className={linkStyle.normal}>
+            <a href="/about" className={linkStyle.selected}>
               about
             </a>
           </div>
           <div className="w-full h-full">
             <div className="w-full h-20 flex flex-col justify-center mb-5">
-              <h1 data-value="Theekshana Fernando" className="name text-3xl font-bold text-white font-kaisei">
-                Theekshana Fernando
-              </h1>
+              <h1 className="name text-3xl font-bold text-white font-kaisei">About me</h1>
             </div>
 
             <p className="w-2/3 text-white font-kaisei mb-5">
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,12 +1,19 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'class',
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        kaisei: ['"Kaisei HarunoUmi"', ...defaultTheme.fontFamily.serif]
+      }
+    }
   },
   plugins: [],
 }
